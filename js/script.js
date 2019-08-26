cf4cfb4c3c97dbc49ac39b5fa7a2cbde2d61244d
Bett Baranovsky <black.laufer2000@gmail.com>
2019-08-26 09:35:53 +0200

    Add js/script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 let playerMove = `papier`;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz :)');
