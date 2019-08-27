ecab240650702f1064e8d234d42b248adec45613
Bett Baranovsky <black.laufer2000@gmail.com>
2019-08-27 15:47:02 +0200

    Add function computer js/script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
+function getMoveName(argMoveId){
+    if(argMoveId === 1) {
+      return 'kamień';
+    }
+
+    if(argMoveId === 2) {
+        return 'papier';
+    }
+
+    if(argMoveId === 3) {
+        return 'nozyce';
+    } else {
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nieznany ruch';
+      }
+}
+
+  
+  
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber === 1) {
+/*if(randomNumber === 1) {
   computerMove = 'kamień';
 }
 
@@ -14,7 +33,7 @@ if(randomNumber === 2) {
   
   if(randomNumber === 3) {
     computerMove = 'nożyce';
-  }
+  }*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -22,9 +41,9 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(randomNumber);
 
-if(playerInput === '1') {
+/*if(playerInput === '1') {
   playerMove = 'kamień';
 }
 
@@ -34,7 +53,7 @@ if(playerInput === '2') {
   
   if(playerInput === '3') {
     playerMove = 'nożyce';
-  }
+  }*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
